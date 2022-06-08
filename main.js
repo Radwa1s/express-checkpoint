33deefc584fa182e03e0742df094dbcad856c145
Mac <mac@Macs-MacBook-Pro.local>
2022-06-08 13:47:55 +0200

    express-checkpoint

---
+++ main.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const workingdays = (req, res, next) => {
+  const date = new Date();
+  const day = date.getDay();
+  const hours = date.getHours();
+
+  const dayoff = [0, 6];
+  const closetime = [9, 10, 11, 12, 13, 14, 15, 16];
+
+  if (dayoff.includes(day)) {
+    res.redirect("/closingtime");
+  } else if (!closetime.includes(hours)) {
+    res.redirect("/closehours");
+  }
+
+  return next();
+};
+app.get("/closingtime", (req, res) => {
+  res.render("closingtime");
+});
+
+app.get("/closehours", (req, res) => {
+  res.render("closehours");
+});
+app.use(workingdays);
+
+app.get("/", (req, res) => {
+  res.render("homePage");
+});
+
+app.get("/ourservices", (req, res) => {
+  res.render("ourServices");
+});
+app.get("/contactus", (req, res) => {
+  res.render("contactUs");
+});
+app.listen(2000, () => {
+  console.log("running on 2000");
+});
